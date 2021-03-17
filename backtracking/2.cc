b32498f657ac5328cb2a2854827cd36504d66b02
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-03-17 10:43:41 +0530

    solved n-queen problem

---
+++ backtracking/2.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f(i,k,n) for(int i = 0; i < n; i++)
+#define fo(i,k,n) for(i = 0; i < n; i++)
+
+template<typename T>
+void print(T ele, string message = ""){
+  cout<<message<<ele<<endl;
+}
+
+template<typename T>
+void print(T begin, T end, string message = ""){
+  cout<<message;
+  while(begin < end){
+    cout<<*begin<<" ";
+    begin++;
+  }
+  cout << endl;
+}
+
+
+vector<string> wordBreak(int n, vector<string>& dict, string s) {
+  unordered_set<string> dictionary;
+  for(auto& str : dict){
+    dictionary.insert(str);
+  }
+}
