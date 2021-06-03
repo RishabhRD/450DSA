582bdb2b7bb003097633d49e217d0e86b8191211
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-03 12:43:59 +0530

    added all possible binary trees problem

---
+++ dynamic_programming/33.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+std::ostream &operator<<(std::ostream &os, const is_container auto &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::value_type&{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::value_type&{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+using lli = long long int;
+
+
+long long int countBT(int h) { 
+  constexpr lli modulo = 1000000007;
+  vector<lli> heights(h + 1);
+  auto height = accessor(heights);
+  height(0) = 1;
+  height(1) = 1;
+
+  for(int i = 2; i <= h; i++){
+    height(i) = (2 * (height(i -1) * height(i - 2)) + height(i - 1) * height(i - 1)) % modulo;
+  }
+
+  return height(h);
+}
+
+int main(int argc, const char** argv) {
+  std::cout << countBT(3) << std::endl;
+    return 0;
+}
