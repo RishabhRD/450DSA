d035339234606d123c33feb0423023487d7ef1b3
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-04 16:19:55 +0530

    added coin win the game

---
+++ dynamic_programming/45.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+bool winner(int n, int x, int y){
+  vector<bool> dp(n + 1);
+  auto at = accessor(dp);
+  sol(0) = false;
+  sol(1) = true;
+
+  for(int i = 2; i <= n; i++){
+    if(i - 1 >= 0 && !sol(i - 1)){
+      sol(i) = true;
+    }else if(i - x >= 0 && !sol(i - x)){
+      sol(i) = true;
+    }else if(i - y >= 0 && !sol(i - y)){
+      sol(i) = true;
+    }else{
+      sol(i) = false;
+    }
+  }
+  return sol(n);
+}
