8275c614560293f46e1af866be1928bc8ac5ba73
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-07 22:10:39 +0530

    added find missing and repeating

---
+++ search/6.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+int *findTwoElement(int *arr, int n) {
+  auto at = [&](int i) -> int&{
+    return arr[i - 1];
+  };
+  int i = 1;
+  int dup;
+  while(i <= n){
+    if(i == at(i)){
+      i++;
+      continue;
+    }
+    int j = at(i);
+    if(at(j) == j){
+      dup = j;
+      break;
+    }
+    swap(at(i), at(j));
+  }
+  long long int sum =  0;
+  for(int i = 0; i < n; i++){
+    sum += (long long int)(arr[i]);
+  }
+  long long int missing = dup - sum + ((long long int)(n)*((long long int)(n)+1)/2);
+  static int ans[2];
+  ans[0] = dup;
+  ans[1] = missing;
+  return ans;
+}
