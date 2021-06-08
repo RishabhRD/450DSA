c997eb8ee020f6a4cde41593c5f603511ed152d1
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-08 20:21:39 +0530

    added majority ele

---
+++ search/7.cc
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+int majorityElement(int a[], int size) {
+  int majority_index = 0;
+  int majority_count = 1;
+  for(int i = 1; i < size; i++){
+    if(a[i] == a[majority_index]){
+      majority_count++;
+    }else{
+      majority_count--;
+    }
+    if(majority_count == 0){
+      majority_count = 1;
+      majority_index = i;
+    }
+  }
+
+  int count = 0;
+  int majority_ele = a[majority_index];
+  for(int i = 0; i < size; i++){
+    if(a[i] == majority_ele){
+      count++;
+    }
+  }
+  if(count > size/2){
+    return majority_ele;
+  }else{
+    return -1;
+  }
+}
