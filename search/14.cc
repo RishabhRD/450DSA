e065e11f8e2b22868d2838efa9517744304d25f3
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-09 17:47:51 +0530

    added 0 sum subarray

---
+++ search/14.cc
@@ -0,0 +1,111 @@
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+long long findSubarray(vector<long long>& arr, int n ) {
+  auto at = const_accessor(arr);
+  unordered_map<long long, long long> sum_map;
+  long long sum = 0;
+  long long ans = 0;
+  for(int i = 0; i < n; i++){
+    sum += at(i);
+    if(sum == 0){
+      ans++;
+    }
+    sum_map[sum]++;
+    ans += sum_map[sum] - 1;
+  }
+  return ans;
+}
+
+int main(int argc, const char** argv) {
+  vector<long long> vec = {6,-1,-3,4,-2,2,4,6,-12,-7};
+  int size  =vec.size();
+  cout << findSubarray(vec, size) << endl;
+    return 0;
+}
