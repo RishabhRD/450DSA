86a635eba0cb0d5e9baa0c1ff93b39d0e3a2edf9
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-13 17:33:27 +0530

    added delete node form ll

---
+++ linked_list/18.cc
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = nullptr;
+  }
+};
+
+void delete_node(Node*& head, int key){
+  if(head == nullptr){
+    return;
+  }
+  if(head->data == key){
+    if(head->next == head){
+      head = nullptr;
+    }else{
+      auto cur = head;
+      while(cur->next != head){
+        cur = cur->next;
+      }
+      cur->next = head->next;
+      head = head->next;
+    }
+  }else{
+    auto cur = head;
+    Node* prev = nullptr;
+    while(cur->next != head){
+      if(cur->data == key){
+        break;
+      }
+      prev = cur;
+      cur = cur->next;
+    }
+    prev->next = cur->next;
+  }
+}
