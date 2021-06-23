6b4d20b04327fd708b171c84f7790dec9a0455da
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-23 17:48:05 +0530

    updated largest subtree

--- btree/27.cc
+++ btree/27.cc
@@ -16,9 +16,9 @@ struct Node {
   Node *right;
 };
 
-int largest_subtree(Node* root){
+int largest_subtree(Node* root, int& max_sum){
   if(root == nullptr) return 0;
-  int ls = largest_subtree(root->left);
-  int rs = largest_subtree(root->right);
-  return max({ls, rs, root->data + ls + rs});
+  int ls = largest_subtree(root->left, max_sum);
+  int rs = largest_subtree(root->right, max_sum);
+  return max(max_sum, root->data + ls + rs);
 }
