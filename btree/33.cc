c41ceb6b9417d8ba90726777bd81d537de0de72a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-23 20:47:54 +0530

    added sum tree

---
+++ btree/33.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+};
+
+
+vector<Node*> printAllDups(Node* root) {
+    // Code here
+}
