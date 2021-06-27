ee5a5d918f2c68843a36f79c6f67857e44ddc8f0
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-27 13:35:22 +0530

    added morris traversal method for kth smallest

--- bst/12.cc
+++ bst/12.cc
@@ -14,22 +14,54 @@ struct Node {
   Node *right;
 };
 
-void k_smallest(Node* root, int k, int& n, int& ans){
-  if(root == nullptr || n >= k){
+void k_smallest(Node *root, int k, int &n, int &ans) {
+  if (root == nullptr || n >= k) {
     return;
   }
   k_smallest(root->left, k, n, ans);
   n++;
-  if(n == k){
+  if (n == k) {
     ans = root->data;
     return;
   }
   k_smallest(root->right, k, n, ans);
 }
 
-int KthSmallestElement(Node *root, int K) {
+int kth_smallest(Node *root, int K) {
   int ans = -1;
   int n = 0;
   k_smallest(root, K, n, ans);
   return ans;
+}
+
+int KthSmallestElement(Node *root, int K) {
+  int count = 0;
+  Node *ans = nullptr;
+  auto cur = root;
+  while (cur != nullptr) {
+    if (cur->left == nullptr) {
+      count++;
+      if (count == K) {
+        ans = cur;
+      }
+      cur = cur->right;
+    } else {
+      auto pre = cur->left;
+      while(pre->right != nullptr && pre->right != cur){
+        pre = pre->right;
+      }
+      if(pre->right == nullptr){
+        pre->right = cur;
+        cur = cur->left;
+      }else{
+        pre->right = nullptr;
+        count++;
+        if(count == K) ans = cur;
+        cur = cur->right;
+      }
+    }
+  }
+  if(ans != nullptr)
+    return ans->data;
+  return -1;
 }
