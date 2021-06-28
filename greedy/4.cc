a0d0a960b34afcc33babdad07e32fda2540579e2
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-06-28 18:17:58 +0530

    fractional knapsack problem

---
+++ greedy/4.cc
@@ -0,0 +1,101 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+struct Item {
+  int value;
+  int weight;
+};
+
+double fractionalKnapsack(int w, Item arr[], int n) {
+  auto rate = [](const Item& item) -> double{
+    return ((double) item.value) / ((double) item.weight);
+  };
+
+  sort(arr, arr + n, [&](const auto& a, const auto& b){
+      return rate(a) > rate(b);
+      });
+
+  double ans = 0;
+  int weight_remaining = w;
+  int cur_index = 0;
+  while(w > 0 and cur_index < n){
+    if(weight_remaining >= arr[cur_index].weight){
+      ans += arr[cur_index].value;
+      weight_remaining -= arr[cur_index].weight;
+    }else{
+      ans += rate(arr[cur_index]) * weight_remaining;
+      weight_remaining = 0;
+    }
+    cur_index++;
+  }
+  return ans;
+}
