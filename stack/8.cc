189bc281a1be1593c2548368e4d2ba3b653779ff
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-05 18:21:30 +0530

    added next larger element

---
+++ stack/8.cc
@@ -0,0 +1,103 @@
+#include <unordered_map>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+vector<long long> nextLargerElement(vector<long long> arr, int n) {
+  unordered_map<long long, long long> mp;
+  stack<long long> st;
+  st.push(arr[0]);
+  for(int i = 1; i < n ; i++){
+    if(st.empty()){
+      st.push(arr[i]);
+      continue;
+    }
+    while(!st.empty() and st.top() < arr[i]){
+      mp[st.top()] = arr[i];
+      st.pop();
+    }
+    st.push(arr[i]);
+  }
+  while(!st.empty()){
+    mp[st.top()] = -1;
+    st.pop();
+  }
+  vector<long long> vec(n);
+  for(int i = 0; i < n; i++){
+    vec[i] = mp[arr[i]];
+  }
+  return vec;
+}
