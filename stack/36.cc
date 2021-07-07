74d85b35a344685325d1c1f117f4636b4ac4d966
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-07 20:48:12 +0530

    first non repeating element in stream

---
+++ stack/36.cc
@@ -0,0 +1,122 @@
+#include <list>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept{
+  return N;
+}
+
+string FirstNonRepeating(string A){
+  queue<char> q;
+  vector<int> char_freq(26);
+  string str;
+  for(auto c : A){
+    q.push(c);
+    char_freq[c - 'a']++;
+    while(not q.empty()){
+      char top = q.front();
+      if(char_freq[top - 'a'] > 1){
+        q.pop();
+      }else{
+        str += top;
+        break;
+      }
+    }
+    if(q.empty()){
+      str += '#';
+    }
+  }
+  return str;
+}
+
+int main(int argc, const char** argv) {
+  cout << FirstNonRepeating("aabc") << endl;
+    return 0;
+}
