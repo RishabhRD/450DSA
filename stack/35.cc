cc010da96a807c864544e44f038ae89dd571737a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-07 19:51:22 +0530

    game with string

---
+++ stack/35.cc
@@ -0,0 +1,106 @@
+#include <queue>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+int minValue(string s, int k) {
+  unordered_map<char, int> mp;
+  for(auto c : s){
+    mp[c]++;
+  }
+  priority_queue<int> pq;
+  for(auto& pr : mp){
+    pq.push(pr.second);
+  }
+  for(int i = 0; i < k; i++){
+    int top = pq.top();
+    if(top == 0) break;
+    pq.pop();
+    pq.push(top - 1);
+  }
+  int sum = 0;
+  while(not pq.empty()){
+    int top = pq.top();
+    pq.pop();
+    sum += (top * top);
+  }
+  return sum;
+}
+
+int main(int argc, const char** argv) {
+  cout << minValue("abccc", 1) << endl;
+    return 0;
+}
