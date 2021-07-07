60c8e7de5903fa7d9c053878e731d4619056aeb7
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-07 09:25:18 +0530

    check for redundant bracket

---
+++ stack/18.cc
@@ -0,0 +1,125 @@
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept{
+  return N;
+}
+
+bool checkRedundancy(string& str){
+  stack<char> st;
+  bool last_pop = false;
+  bool last_alpha = false;
+  for(auto c : str){
+    if(c == ')'){
+      bool any_char_found = false;
+      bool operator_found = false;
+      while(!st.empty()){
+        char top = st.top();
+        if(top == '*' or top == '+' or top == '-' or top == '/'){
+          any_char_found = true;
+          operator_found = true;
+        }else if(top == '('){
+          if(any_char_found == false){
+            return true;
+          }
+          if(operator_found == false){
+            return true;
+          }
+        }else{
+          any_char_found = true;
+        }
+        st.pop();
+      }
+    }else{
+      st.push(c);
+    }
+  }
+  return false;
+}
