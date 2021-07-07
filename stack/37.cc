61a959135f7a8fb11f9ff6a5811f45d6d4e1fec1
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-07 21:23:00 +0530

    next smaller element

---
+++ stack/37.cc
@@ -0,0 +1,120 @@
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+template <typename T> concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>requires (!std::same_as<T, std::string>)
+std::ostream &operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T>
+constexpr auto accessor(T& t){
+  return [&](int i) -> typename T::reference{
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto accessor(const T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto const_accessor(T& t){
+  return [&](int i){
+    return t.at(i);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(T& t){
+  return [&](int i, int j) -> typename T::value_type::reference{
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto matrix_accessor(const T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+constexpr auto const_matrix_accessor(T& t){
+  return [&](int i, int j) {
+    return t.at(i).at(j);
+  };
+}
+
+template <typename T>
+using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept{
+  return N;
+}
+
+vector<int> next_smaller(const vector<int>& vec){
+  const int size = vec.size();
+  stack<int> st;
+  vector<int> ret;
+  for(int i = size - 1; i >= 0; i--){
+    if(st.empty()){
+      ret.push_back(-1);
+    }else{
+      while(not st.empty()){
+        const int top = st.top();
+        if(vec[i] > top) break;
+        else st.pop();
+      }
+      if(st.empty()) ret.push_back(-1);
+      else ret.push_back(st.top());
+    }
+    st.push(vec[i]);
+  }
+  reverse(begin(ret), end(ret));
+  return ret;
+}
+
+int main(int argc, const char** argv) {
+  cout << next_smaller({13, 7, 6, 12}) << endl;
+    return 0;
+}
