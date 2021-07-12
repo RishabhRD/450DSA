523fe57cc80263356c10961dc410f35a3921f2be
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-12 18:50:50 +0530

    bst to min heap

---
+++ heap/14.cc
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+struct node {
+  node *left, *right;
+  int val;
+};
+
+template <typename Func> void inorder(node *root, Func &&func) {
+  if (root == nullptr)
+    return;
+  inorder(root->left, func);
+  func(root);
+  inorder(root->right, func);
+}
+
+template <typename Func> void preorder(node *root, Func &&func) {
+  if (root == nullptr)
+    return;
+  func(root);
+  preorder(root->left, func);
+  preorder(root->right, func);
+}
+
+void make_min_heap(node *root) {
+  vector<int> inorder_ele;
+  inorder(root, [&](node *ele) { inorder_ele.push_back(ele->val); });
+  int cur = 0;
+  preorder(root, [&](node *ele) { ele->val = inorder_ele[cur++]; });
+}
