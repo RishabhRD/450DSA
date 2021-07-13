3d7073296e4a5b0215027e0beeb2d0144a9e60bd
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-13 13:02:13 +0530

    bfs of graph

---
+++ heap/11.cc
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+class Solution {
+  vector<int> vec;
+public:
+  // Function to insert heap.
+  void insertHeap(int &x) {
+    vec.insert(upper_bound(cbegin(vec), cend(vec), x), x);
+  }
+
+  // Function to balance heaps.
+  void balanceHeaps() {}
+
+  // Function to return Median.
+  double getMedian() {
+    if(vec.size() % 2){
+      return vec[vec.size() / 2];
+    }else{
+      return ((double)vec[vec.size() / 2 - 1] + (double)vec[vec.size() / 2]) / 2;
+    }
+  }
+};
+
+int main(int argc, const char** argv) {
+  Solution sol;
+  vector vec = {5, 10, 15};
+  for(auto x : vec){
+    sol.insertHeap(x);
+    cout << sol.getMedian() << endl;
+  }
+    return 0;
+}
