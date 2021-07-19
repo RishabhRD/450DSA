54657c305ad1fb7d160293d3e9d96f16dfa7d360
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-19 20:08:05 +0530

    topological sort

---
+++ graph/12.cc
@@ -0,0 +1,102 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+template <typename Func>
+void dfs(int cur, vector<int>* adj, vector<bool>& visited, Func&& func){
+  visited[cur] = true;
+  for(auto x : adj[cur]){
+    if(not visited[x]){
+      dfs(x, adj, visited, func);
+    }
+  }
+  func(cur);
+}
+
+vector<int> topoSort(int V, vector<int> adj[]) {
+  vector<int> ret_vector;
+  auto push_to_vector = [&](const int x){
+    ret_vector.push_back(x);
+  };
+  vector<bool> visited(V);
+  for(int i = 0; i < V; i++){
+    if(not visited[i]){
+      dfs(i, adj, visited, push_to_vector);
+    }
+  }
+  reverse(begin(ret_vector), end(ret_vector));
+  return ret_vector;
+}
