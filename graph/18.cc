94d6b48d8f685b48099a485633ac0394875b8623
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-22 13:57:31 +0530

    added prim's

---
+++ graph/18.cc
@@ -0,0 +1,102 @@
+#include <queue>
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+concept is_container = requires(T a) {
+  a.begin();
+  a.end();
+};
+
+template <is_container T>
+requires(!std::same_as<T, std::string>) std::ostream &
+operator<<(std::ostream &os, const T &cont) {
+  os << '{';
+  for (const auto &x : cont) {
+    os << x;
+    os << ' ';
+  }
+  os << '}';
+  return os;
+}
+
+void printMatrix(const is_container auto &cont) {
+  for (const auto &x : cont) {
+    cout << x;
+    cout << '\n';
+  }
+}
+
+template <typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+create_array(T value, std::index_sequence<Is...>) {
+  return {{(static_cast<void>(Is), value)...}};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<T, N> create_array(const T &value) {
+  return create_array(value, std::make_index_sequence<N>());
+}
+template <typename T> constexpr auto accessor(T &t) {
+  return [&](int i) -> typename T::reference { return t.at(i); };
+}
+
+template <typename T> constexpr auto accessor(const T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto const_accessor(T &t) {
+  return [&](int i) { return t.at(i); };
+}
+
+template <typename T> constexpr auto matrix_accessor(T &t) {
+  return [&](int i, int j) ->
+         typename T::value_type::reference { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto matrix_accessor(const T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> constexpr auto const_matrix_accessor(T &t) {
+  return [&](int i, int j) { return t.at(i).at(j); };
+}
+
+template <typename T> using lmt = std::numeric_limits<T>;
+
+template <typename T, std::size_t N>
+constexpr std::size_t array_size(const T (&)[N]) noexcept {
+  return N;
+}
+
+int spanningTree(int n, const vector<vector<int>> adj[]) {
+  constexpr static int inf = INT32_MAX;
+  using pint = pair<int, int>;
+  priority_queue<pint, vector<pint>, greater<pint>> pq;
+  vector<int> key(n, inf);
+  vector<bool> in_mst(n);
+  key[0] = 0;
+  pq.emplace(0, 0);
+  while(not pq.empty()){
+    auto top = pq.top();
+    pq.pop();
+    if(in_mst[top.second]) continue;
+    in_mst[top.second] = true;
+    for(auto& pr : adj[top.second]){
+      int v = pr[0];
+      int wt = pr[1];
+      if(in_mst[v] == false and key[v] > wt){
+        key[v] = wt;
+        pq.emplace(wt, v);
+      }
+    }
+  }
+  return accumulate(cbegin(key), cend(key), 0);
+}
